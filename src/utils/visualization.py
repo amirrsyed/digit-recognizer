dcfb951e266078c720dc200c273aa288bc2bd245
Amir Syed <75707901+amirrsyed@users.noreply.github.com>
2026-04-25 03:57:21 -0400

    Update visualization.py

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+def plot_history(history):
+    history_frame = pd.DataFrame(history.history)
+
+    history_frame[['loss', 'val_loss']].plot()
+    plt.title("Loss")
+    plt.savefig("outputs/figures/loss.png")
+
+    history_frame[['accuracy', 'val_accuracy']].plot()
+    plt.title("Accuracy")
+    plt.savefig("outputs/figures/accuracy.png")
